09aa9cdc859bd08b03ae3c6df8041f2bff873d89
Jozsef Molnar <mol20321@spengergasse.at>
2025-03-14 17:21:06 +0100

    Environments create

---
+++ frontend/MolnarWMC-Web/src/environments/environment.ts
@@ -0,0 +1,12 @@
+export const environment = {
+    production: false,
+    firebaseConfig: {
+      apiKey: "AIzaSyBK9oDCepHIK2dIzFLNHwBZQGcowQrTkbI",
+      authDomain: "molnarwmc.firebaseapp.com",
+      projectId: "molnarwmc",
+      storageBucket: "molnarwmc.firebasestorage.app",
+      messagingSenderId: "1083387675634",
+      appId: "1:1083387675634:web:9accb1038caec2d3fea16b"
+    }
+  };
+  
